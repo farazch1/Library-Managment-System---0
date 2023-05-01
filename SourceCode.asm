eb9cbe03fc61c41692c0def76bfe6da0e4450a0f
farazch1 <101258532+farazch1@users.noreply.github.com>
2023-05-02 00:29:55 +0500

    Update SourceCode.asm

--- SourceCode.asm
+++ SourceCode.asm
@@ -32,7 +32,7 @@ msg1	 BYTE 0AH
 REG_MSG	 BYTE "	Enter Member's Name to register: ",0
 VIEW_MEMBERS_MSG BYTE 0Ah,"	Viewing Registered Members: ",0AH, 0DH, 0
 ADD_MSG			 BYTE "	Enter Book Name & Author Name to Add: ", 0dh, 0ah,
-			 "	Separated By Comma:",0
+			 "	Separated By Comma and Write semi-colon and give space before entering: ",0
 VIEW_BOOKS_MSG BYTE 0Ah, "	Viewing Books in Library: ",  0dh, 0ah, 0
 EXIT_MSG	   BYTE 0AH,
 				    "	----------------- ",0dh, 0ah,
@@ -46,6 +46,10 @@ bool		   DWORD ?
 MEMBERS_FILE   BYTE "C:\Users\faraz\Downloads\Compressed\Library-Management-System-master\Library-Management-System-master\MEMBERS.txt",0
 BOOKS_FILE     BYTE "C:\Users\faraz\Downloads\Compressed\Library-Management-System-master\Library-Management-System-master\BOOKS.txt",0
 filehandle     DWORD ?
+filehandle_B     DWORD ?
+Book_name_size   DWORD ?
+m_sizee     DWORD ?
+
 BUFFER_SIZE = 5000
 buffer_mem   BYTE buffer_size DUP (?)
 buffer_book  BYTE buffer_size DUP (?)
@@ -104,55 +108,6 @@ main PROC
 	JE VIEW_BFILE		; taking input in 2D array
 		JMP EXIT_MENU
 
-;----------------------------------------
-;------------REGISTER MEMBERS------------
-;----------------------------------------
-	REG_M:
-		INVOKE MSG_DISPLAY, ADDR REG_MSG          ;INVOKE STRING_INPUT, ADDR INPUT_STRING
-
-	MOV ESI, OFFSET MEMBERS
-	MOV EAX, MEMBER_SIZE
-	MUL NUM_MEMBERS
-	ADD ESI, EAX
-	MOV EDX, ESI
-	MOV ECX, MEMBER_SIZE
-	CALL READSTRING
-	INC NUM_MEMBERS
-
-	;INVOKE CreateFile,
-	;ADDR MEMBERS_FILE,
-	;GENERIC_WRITE,
-	;DO_NOT_SHARE, 
-	;NULL, 
-	;OPEN_ALWAYS, 
-	;FILE_ATTRIBUTE_NORMAL, 
-	;0
-;
-;cmp eax, INVALID_HANDLE_VALUE
-	;je exit_1
-	;mov filehandle, eax
-;INVOKE SetFilePointer,
-	 ;filehandle,
-	;0, ; distance low
-	;0, ; distance high
-	;FILE_END
-	;mov eax,filehandle
-;
-	;mov edx, offset BUFFER_MEM
-	;mov ecx, 7
-	;call READSTRING
-	;mov eax, filehandle
-	;call WriteToFile
-;
-	;INVOKE SetFilePointer,
-	 ;filehandle,
-	;0, ; distance low
-	;0, ; distance high
-	;FILE_END
-;exit_1:
-	;invoke CloseHandle, filehandle
-
-		JMP START
 
 ;--------------------------------------
 ;--------------VIEW MEMBERS------------
@@ -178,41 +133,51 @@ LOOP OUTPUT
 ; VIEW MEMBERS FROM FILE
 VIEW_MFILE:
 	INVOKE CreateFile,
-	ADDR MEMBERS_FILE, ; ptr to filename
-	GENERIC_READ, ; mode = Can read
-	DO_NOT_SHARE, ; share mode
-	NULL, ; ptr to security attributes
-	OPEN_ALWAYS, ; open an existing file
-	FILE_ATTRIBUTE_NORMAL, ; normal file attribute
-	0 ; not used
+	    ADDR MEMBERS_FILE, ; ptr to filename
+	    GENERIC_READ, ; mode = Can read
+	    DO_NOT_SHARE, ; share mode
+	    NULL, ; ptr to security attributes
+	    OPEN_ALWAYS, ; open an existing file
+	    FILE_ATTRIBUTE_NORMAL, ; normal file attribute
+	    0 ; not used
 	mov filehandle, eax ; Copy handle to variable
+	
 	invoke ReadFile,
-	filehandle, ; file handle
-	addr BUFFER_MEM, ; where to read
-	BUFFER_SIZE, ; num bytes to read
-	addr bytesRead, ; bytes actually read
-	0
+	    filehandle, ; file handle
+	    addr BUFFER_MEM, ; where to read
+	    BUFFER_SIZE, ; num bytes to read
+	    addr bytesRead, ; bytes actually read
+	    0
 	invoke CloseHandle,
 	filehandle
 	mov edx, offset BUFFER_MEM ; Write String
 	call WriteString
 	JMP START
-;----------------------------------
-;--------------ADD BOOKS-----------
-;----------------------------------	
-	ADD_B:
 
-	INVOKE MSG_DISPLAY, ADDR ADD_MSG
-	MOV ESI, OFFSET BOOKS
-	MOV EAX, BOOK_SIZE
-	MUL NUM_BOOKS
+;----------------------------------------
+;------------REGISTER MEMBERS------------
+;----------------------------------------
+	REG_M:
+		INVOKE MSG_DISPLAY, ADDR REG_MSG          ;INVOKE STRING_INPUT, ADDR INPUT_STRING
+
+	MOV ESI, OFFSET MEMBERS
+	MOV EAX, MEMBER_SIZE
+	MUL NUM_MEMBERS
 	ADD ESI, EAX
 	MOV EDX, ESI
-	MOV ECX, BOOK_SIZE
+	MOV ECX, MEMBER_SIZE
 	CALL READSTRING
-	INC NUM_BOOKS
-		
+	mov m_sizee,eax
+	INC NUM_MEMBERS
+
+	INVOKE CreateFile, ADDR MEMBERS_FILE, FILE_APPEND_DATA, 0, 0, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, 0
+    MOV filehandle, EAX
+ 
+	INVOKE WriteFile, filehandle,ESI, m_sizee, ADDR bytesRead, NULL
+
 	JMP START
+
+
 ;------------------------------------
 ;-------------VIEW BOOKS-------------
 ;------------------------------------
@@ -245,17 +210,40 @@ VIEW_BFILE:
 	OPEN_ALWAYS, ; open an existing file
 	FILE_ATTRIBUTE_NORMAL, ; normal file attribute
 	0 ; not used
-	mov filehandle, eax ; Copy handle to variable
+	mov filehandle_B, eax ; Copy handle to variable
+	
 	invoke ReadFile,
-	filehandle, ; file handle
+	filehandle_B, ; file handle
 	addr BUFFER_BOOK, ; where to read
 	BUFFER_SIZE, ; num bytes to read
 	addr bytesRead, ; bytes actually read
 	0
+	
 	invoke CloseHandle,
-	filehandle
+	filehandle_B
 	mov edx, offset BUFFER_BOOK ; Write String
 	call WriteString
+	JMP START
+;----------------------------------
+;--------------ADD BOOKS-----------
+;----------------------------------	
+	ADD_B:
+	INVOKE MSG_DISPLAY, ADDR ADD_MSG
+	MOV ESI, OFFSET BOOKS
+	MOV EAX, BOOK_SIZE
+	MUL NUM_BOOKS
+	ADD ESI, EAX
+	MOV EDX, ESI
+	MOV ECX, BOOK_SIZE
+	CALL READSTRING
+	mov Book_name_size,EAX 
+	INC NUM_BOOKS
+    
+	INVOKE CreateFile, ADDR BOOKS_FILE, FILE_APPEND_DATA, 0, 0, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, 0
+    MOV filehandle_B, EAX
+ 
+	INVOKE WriteFile, filehandle_B, ESI, Book_name_size, ADDR bytesRead, NULL
+		
 	JMP START
 ;-------------------------------------------
 ;----------------EXIT MENU------------------
